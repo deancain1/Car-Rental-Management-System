f2946234bcf00fc657f8d73a2336333cad2bc1e2
deancain1 <caindeanw@gmail.com>
2024-12-04 11:05:51 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Carl_Rental_System.Forms.Admin_Interface;
+using Carl_Rental_System.Forms.User_Interface;
+using System;
+using System.Windows.Forms;
+
+namespace Carl_Rental_System
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new UserLogIn());
+        }
+    }
+}
