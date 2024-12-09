cc4a2cbab1d3c69a9083bca2a8c13e57a63bae56
Dean Cain A. Ronquillo <caindeanw@gmail.com>
2024-12-09 21:41:52 +0800

    Add files via upload

---
+++ Database/ReturnedCarDB.cs
@@ -0,0 +1,70 @@
+﻿using Carl_Rental_System.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Carl_Rental_System.Database
+{
+    public class ReturnedCarDB
+    {
+        private static string ConnectionString = @"Data Source=LAPTOP-S6PGLPQ2;Initial Catalog = carRentalDB; Integrated Security = True;";
+        public List<ReturnedCarModel> LoadCarsInDGV()
+        {
+            List<ReturnedCarModel> carsModels = new List<ReturnedCarModel>();
+
+            string query = "SELECT ReturnedCarID,  FullName, Role, PhoneNumber, Model, Price, DailyRate, Total, Gas, Transmission, CarImage, Quantity, PickUpDate, DropOffDate FROM ReturnedCar";
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    try
+                    {
+               
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ReturnedCarModel carModel = new ReturnedCarModel
+                                {
+                                    ReturnedCarID = reader.GetInt32(reader.GetOrdinal("ReturnedCarID")),
+                                    FullName = reader.GetString(reader.GetOrdinal("FullName")),
+                                    Role = reader.GetString(reader.GetOrdinal("Role")),
+                                    PhoneNumber = reader.GetString(reader.GetOrdinal("PhoneNumber")),
+                                    Model = reader.GetString(reader.GetOrdinal("Model")),
+                                    Price = reader.GetDecimal(reader.GetOrdinal("Price")),
+                                    DailyRate = reader.GetDecimal(reader.GetOrdinal("DailyRate")),
+                                    Total = reader.GetDecimal(reader.GetOrdinal("Total")),
+                                    Gas = reader.GetString(reader.GetOrdinal("Gas")),
+                                    Transmission = reader.GetString(reader.GetOrdinal("Transmission")),
+                                    Quantity = reader.GetInt32(reader.GetOrdinal("Quantity")),
+                                    PickUpDate = reader.GetDateTime(reader.GetOrdinal("PickUpDate")),
+                                    DropOffDate = reader.GetDateTime(reader.GetOrdinal("DropOffDate")),
+                                    CarImage = (byte[])reader["CarImage"]
+
+                                };
+
+                                carsModels.Add(carModel);
+                            }
+                        }
+                    }
+                    catch (SqlException sqlEx)
+                    {
+                        MessageBox.Show("SQL error occurred: " + sqlEx.Message);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An error occurred: " + ex.Message);
+                    }
+                }
+            }
+
+            return carsModels;
+        }
+    }
+}
